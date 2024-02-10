64ac3db2a74d51631b8a4cbe8614ab0f71eb986f
Евгений Шкумат <kotbmasle@gmail.com>
2024-02-10 09:09:34 +0300

    add and use is_matrix_unit

--- Project01/Logic.cpp
+++ Project01/Logic.cpp
@@ -2,9 +2,9 @@
 
 bool is_matrix_null(int matrix[N][N]) {
 	bool null = true; // 1
-
+	//1
 	for (int i = 0; i < N; i++) //  2
-	{
+	{ //1
 		for (int j = 0; j < N; j++)//  2
 		{
 			if (matrix[i][j] != 0) {// 1
@@ -17,24 +17,47 @@ bool is_matrix_null(int matrix[N][N]) {
 	return null; // 1
 }
 //worst: O(57)
-//O(6n^2 + 2n + 3)
+//O(6n^2 + 2n + 4)
 
 bool is_matrix_quadratic(int matrix[N][N]) {
-	bool quadratic = true;
+	bool quadratic = true;//1
+	//1
+	for (int i = 0; i < N; i++)//2
+	{ //1
+		for (int j = 0; j < N; j++)//2
+		{
+			if (matrix[i][j] != 0 && i != j) {//3
+				quadratic = false;//1
+				break;//1
+			}
+		}
+		if (!quadratic) {//1
+			break;//1
+		}
+	}
+
+	return quadratic;//1
+}
+//best: O(21)
+//worst: O(60)
+//O(6n^2 + 3n + 3)
+
+bool is_matrix_unit(int matrix[N][N]) {
+	bool unit = true;
 	
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < N; j++)
 		{
-			if (matrix[i][j] != 0 && i != j) {
-				quadratic = false;
+			if ((matrix[i][j] != 0 && i != j) || (matrix[i][j] != 1 && i == j)) {
+				unit = false;
 				break;
 			}
 		}
-		if (!quadratic) {
+		if (!unit) {
 			break;
 		}
 	}
 
-	return quadratic;
+	return unit;
 }
--- Project01/Main.cpp
+++ Project01/Main.cpp
@@ -6,13 +6,14 @@ string convert_matrix_to_string(int matrix[N][N]);
 
 bool is_matrix_null(int matrix[N][N]);
 bool is_matrix_quadratic(int matrix[N][N]);
+bool is_matrix_unit(int matrix[N][N]);
 
 int main() {
-	int matrix[N][N] = { {1, 0, 0}, {0, 2, 3}, {0, 0, 3} };
+	int matrix[N][N] = { {1, 0, 0}, {0, 1, 0}, {0, 0, 1} };
 
 	cout << "Matrix:\n" << convert_matrix_to_string(matrix);
 
-	cout << (is_matrix_quadratic(matrix) ? "Yes it is quadratic" : "No it is not quadratic");
+	cout << (is_matrix_unit(matrix) ? "Yes it is unit" : "No it is not unit");
 
 	return 0;
 }
